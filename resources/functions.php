25f9cdae63096b94fa38b93a0521133ef578ebfe
mirzahab85 <mirza.habul@telemach.ba>
2021-05-07 00:53:26 +0200

    Display Products

--- resources/functions.php
+++ resources/functions.php
@@ -54,8 +54,25 @@ confirm($query);
 
 while($row = fetch_array($query)) {
 
+$product = <<<DELIMETER
 
-echo $row['product_price'];
+<div class="col-sm-4 col-lg-4 col-md-4">
+    <div class="thumbnail">
+        <img src="http://placehold.it/320x150" alt="">
+        <div class="caption">
+            <h4 class="pull-right">{$row['product_price']}</h4>
+            <h4><a href="product.html">First Product</a>
+            </h4>
+            <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+             <a class="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">View Tutorial</a>
+        </div>
+                                    
+    </div>
+</div>
+
+DELIMETER;
+
+echo $product;
 
 }
 
