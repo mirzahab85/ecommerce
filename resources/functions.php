8479e4ae3eefcdceb748df6eeba4743c9a5524d8
mirzahab85 <mirza.habul@telemach.ba>
2021-05-10 22:41:43 +0200

    Single Product Page Setup

--- resources/functions.php
+++ resources/functions.php
@@ -45,6 +45,9 @@ return mysqli_fetch_array($result);
 
 }
 
+
+/***********************************FRONT END FUNCTIONS*********************************/
+
 // get products
 
 function get_products() {
@@ -61,7 +64,7 @@ $product = <<<DELIMETER
         <a href="item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
         <div class="caption">
             <h4 class="pull-right">&#36;{$row['product_price']}</h4>
-            <h4><a href="product.html">{$row['product_title']}</a>
+            <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
             </h4>
             <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
              <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to cart</a>
@@ -96,5 +99,8 @@ echo $categories_links;
         }
 
 }
+
+
+/***********************************BACK END FUNCTIONS*********************************/
 
 ?>
