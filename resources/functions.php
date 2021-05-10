2e98af4f795989165138beb08b43c967dd91d846
mirzahab85 <mirza.habul@telemach.ba>
2021-05-10 22:18:30 +0200

    Category Page and Function

--- resources/functions.php
+++ resources/functions.php
@@ -74,9 +74,26 @@ DELIMETER;
 
 echo $product;
 
+        }
+
 }
 
+function get_categories(){
 
+$query = query("SELECT * FROM categories");
+confirm($query);
+
+while ($row = fetch_array($query)) {
+
+$categories_links = <<<DELIMETER
+
+<a href='category.php?id={$row['cat_id']}' class='list-group-item'>{$row['cat_title']}</a>
+
+DELIMETER;
+
+echo $categories_links;
+
+        }
 
 }
 
