50a5dfa40ed2b388c883433f6dd76056ec9bbf58
mirzahab85 <mirza.habul@telemach.ba>
2021-05-12 00:22:14 +0200

    Displaying Products in Category Page

--- database/ecom_db.sql
+++ database/ecom_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2021 at 11:45 PM
+-- Generation Time: May 12, 2021 at 12:21 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
