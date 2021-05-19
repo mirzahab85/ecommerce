5b6c7f087ec05b5213588b4ae46c9b6778a65b52
mirzahab85 <mirza.habul@telemach.ba>
2021-05-19 21:42:12 +0200

    CART function part 1

--- public/cart.php
+++ public/cart.php
@@ -49,5 +49,35 @@ if(isset($_GET['delete'])) {
     redirect("checkout.php");
 }
 
+function cart() {
+
+$query = query("SELECT * FROM products");
+confirm($query);
+
+while ($row = fetch_array($query)) {
+
+$product = <<<DELIMETER
+
+<tr>
+    <td>{$row['product_title']}</td>
+    <td>$23</td>
+    <td>3</td>
+    <td>2</td>
+    <td><a href="cart.php?remove=1">Remove</a></td>
+    <td><a href="cart.php?delete=1">Delete</a></td>
+    </tr>
+
+DELIMETER;
+
+echo $product;
+
+}
+
+
+
+
+
+}
+
 
 ?>
