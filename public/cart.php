7150b4d91d201071089cbd785a6e3ddda86f2aa6
mirzahab85 <mirza.habul@telemach.ba>
2021-05-20 23:09:22 +0200

    CART function part 5

--- public/cart.php
+++ public/cart.php
@@ -57,22 +57,24 @@ if($value > 0) {
 
 if(substr($name, 0, 8) == "product_") {
 
-$length = strlen($name - 8);
+$length = strlen($name) - 8;
 
 $id = substr($name, 8 , $length);
     
-$query = query("SELECT * FROM products WHERE product_id = " . escape_string($id) . " ");
+$query = query("SELECT * FROM products WHERE product_id = " . escape_string($id). " ");
 confirm($query);
 
 while ($row = fetch_array($query)) {
+
+$sub = $row['product_price']*$value;
 
 $product = <<<DELIMETER
 
 <tr>
     <td>{$row['product_title']}</td>
-    <td>$23</td>
-    <td>3</td>
-    <td>2</td>
+    <td>&#36;{$row['product_price']}</td>
+    <td>{$value}</td>
+    <td>&#36;{$sub}</td>
     <td><a class='btn btn-warning' href="cart.php?remove={$row['product_id']}"><span class='glyphicon glyphicon-minus'></span></a>  <a class='btn btn-success' href="cart.php?add={$row['product_id']}"><span class='glyphicon glyphicon-plus'></span></a>
     <a class='btn btn-danger' href="cart.php?delete={$row['product_id']}"><span class='glyphicon glyphicon-remove'></span></a></td>
     </tr>
