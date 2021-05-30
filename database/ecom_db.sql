958dfd99b4b1fd5838fce2da612e1b9da24398cd
mirzahab85 <mirza.habul@telemach.ba>
2021-05-30 22:26:33 +0200

    Insert New Column in the Orders table - CALLENGE

--- database/ecom_db.sql
+++ database/ecom_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2021 at 10:28 PM
+-- Generation Time: May 30, 2021 at 10:25 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -48,6 +48,7 @@ INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
 
 CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
   `order_amount` float NOT NULL,
   `order_transaction` varchar(255) NOT NULL,
   `order_status` varchar(255) NOT NULL,
@@ -58,10 +59,11 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`order_id`, `order_amount`, `order_transaction`, `order_status`, `order_currency`) VALUES
-(1, 345, 'USA', '34535434', 'Completed'),
-(2, 345, 'USA', '34535434', 'Completed'),
-(3, 345, 'USA', '34535434', 'Completed');
+INSERT INTO `orders` (`order_id`, `product_id`, `order_amount`, `order_transaction`, `order_status`, `order_currency`) VALUES
+(1, 0, 345, 'USA', '34535434', 'Completed'),
+(2, 0, 345, 'USA', '34535434', 'Completed'),
+(3, 0, 345, 'USA', '34535434', 'Completed'),
+(4, 0, 345, 'USA', '34535434', 'Completed');
 
 -- --------------------------------------------------------
 
@@ -85,8 +87,8 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_title`, `product_category_id`, `product_price`, `product_quantity`, `product_description`, `short_desc`, `product_image`) VALUES
-(1, 'product 1', 1, 24.99, 3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac bibendum quam. Fusce id mauris sit amet augue consectetur aliquet eu at odio. Morbi laoreet mi id lobortis sagittis. Quisque non neque ut eros varius interdum. Fusce sit amet risus mollis, luctus est id, egestas elit. Cras nisl mi, luctus sit amet lorem quis, scelerisque laoreet nunc. Sed in blandit arcu, ultrices mollis eros. Proin urna risus, faucibus nec sodales eu, consequat at felis. Nulla facilisi. Integer ac ultrices massa. Aenean sed augue sit amet ex suscipit pretium. Nullam imperdiet a libero quis rutrum. Pellentesque accumsan orci urna, vel fringilla lectus aliquet eget.', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis ', 'http://placehold.it/320x150'),
-(2, 'product 2', 1, 299.99, 2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac bibendum quam. Fusce id mauris sit amet augue consectetur aliquet eu at odio. Morbi laoreet mi id lobortis sagittis. Quisque non neque ut eros varius interdum. Fusce sit amet risus mollis, luctus est id, egestas elit. Cras nisl mi, luctus sit amet lorem quis, scelerisque laoreet nunc. Sed in blandit arcu, ultrices mollis eros. Proin urna risus, faucibus nec sodales eu, consequat at felis. Nulla facilisi. Integer ac ultrices massa. Aenean sed augue sit amet ex suscipit pretium. Nullam imperdiet a libero quis rutrum. Pellentesque accumsan orci urna, vel fringilla lectus aliquet eget.', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ', 'http://placehold.it/320x150');
+(1, 'product 1', 1, 24.99, 10, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac bibendum quam. Fusce id mauris sit amet augue consectetur aliquet eu at odio. Morbi laoreet mi id lobortis sagittis. Quisque non neque ut eros varius interdum. Fusce sit amet risus mollis, luctus est id, egestas elit. Cras nisl mi, luctus sit amet lorem quis, scelerisque laoreet nunc. Sed in blandit arcu, ultrices mollis eros. Proin urna risus, faucibus nec sodales eu, consequat at felis. Nulla facilisi. Integer ac ultrices massa. Aenean sed augue sit amet ex suscipit pretium. Nullam imperdiet a libero quis rutrum. Pellentesque accumsan orci urna, vel fringilla lectus aliquet eget.', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis ', 'http://placehold.it/320x150'),
+(2, 'product 2', 1, 299.99, 10, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac bibendum quam. Fusce id mauris sit amet augue consectetur aliquet eu at odio. Morbi laoreet mi id lobortis sagittis. Quisque non neque ut eros varius interdum. Fusce sit amet risus mollis, luctus est id, egestas elit. Cras nisl mi, luctus sit amet lorem quis, scelerisque laoreet nunc. Sed in blandit arcu, ultrices mollis eros. Proin urna risus, faucibus nec sodales eu, consequat at felis. Nulla facilisi. Integer ac ultrices massa. Aenean sed augue sit amet ex suscipit pretium. Nullam imperdiet a libero quis rutrum. Pellentesque accumsan orci urna, vel fringilla lectus aliquet eget.', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ', 'http://placehold.it/320x150');
 
 -- --------------------------------------------------------
 
@@ -106,7 +108,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `email`, `password`) VALUES
-(1, 'rico', 'rico@hotmail.com', '123');
+(1, 'rico', 'rico@hotmail.com', '123'),
+(5, 'miki', 'miki@test.com', 'asd123');
 
 --
 -- Indexes for dumped tables
@@ -150,7 +153,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -162,7 +165,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
