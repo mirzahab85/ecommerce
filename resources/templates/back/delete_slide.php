94802d5e6c674f9c47f97c54f1a45b377453cfb4
mirzahab85 <mirza.habul@telemach.ba>
2021-07-31 20:22:37 +0200

    Deleting Files and Records - CHALLENGE

--- resources/templates/back/delete_slide.php
+++ resources/templates/back/delete_slide.php
@@ -6,6 +6,9 @@ if(isset($_GET['delete_slide_id'])) {
 $query = query("DELETE FROM slides WHERE slide_id = " . escape_string($_GET['delete_slide_id']) . " ");
 confirm($query);
 
+$target_path = "../../resources/uploads/";
+unlink($target_path);
+
 set_message("Slide Deleted");
 redirect("index.php?slides");
 
