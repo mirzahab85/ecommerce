d34309baee2315aeec735000c14d79d23f67ec2a
mirzahab85 <mirza.habul@telemach.ba>
2021-07-31 21:35:40 +0200

    Deleting Files and Records - SOLUTION

--- resources/templates/back/delete_slide.php
+++ resources/templates/back/delete_slide.php
@@ -1,13 +1,18 @@
 <?php require_once("../../resources/config.php");
 
-
 if(isset($_GET['delete_slide_id'])) {
+
+
+$query_find_image = query("SELECT slide_image FROM slides WHERE slide_id = " . escape_string($_GET['delete_slide_id']) . " LIMIT 1 ");
+confirm($query_find_image);
+
+$row = fetch_array($query_find_image);
+$target_path = UPLOAD_DIRECTORY . DS . $row['slide_image'];
+
+unlink($target_path);
 
 $query = query("DELETE FROM slides WHERE slide_id = " . escape_string($_GET['delete_slide_id']) . " ");
 confirm($query);
-
-$target_path = "../../resources/uploads/";
-unlink($target_path);
 
 set_message("Slide Deleted");
 redirect("index.php?slides");
@@ -17,10 +22,3 @@ redirect("index.php?slides");
 redirect("index.php?slides");
 
 }
-
-
-
-
-
-
-?>
